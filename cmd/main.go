a004bbad5950160e9269bc10f15bd1013ce87b86
PawelHarasiuk <pharasiuk01@gmail.com>
2024-10-18 13:13:02 +0200

    configured aws lambda and created dockerfile

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"EmailExchangeRate/emailHandler"
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
 	"os"
 )
 
@@ -13,20 +16,32 @@ var (
 	smtpPort = "587"
 )
 
-func main() {
+type MyEvent struct {
+	Mess string `json:"mess"`
+}
+
+func handler(ctx context.Context, event MyEvent) {
 	ms := emailHandler.MailSender{
 		From:     from,
 		Passwd:   passwd,
 		SmtpHost: smtpHost,
 		SmtpPort: smtpPort,
 	}
+	message := event.Mess
+	if message == "" {
+		message = "Hello world"
+	}
 
 	subject := "Subject: Test Email"
-	emailBody := "This is the email body."
+	emailBody := fmt.Sprintf("This is the email body: %v", message)
 	to := []string{
 		from,
 		"pharasiuk01@gmail.com",
 	}
 
 	ms.SendMessage(subject, emailBody, to)
+}
+
+func main() {
+	lambda.Start(handler)
 }
