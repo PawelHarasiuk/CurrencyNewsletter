05f667c312a2bc47b2fbfc2bc36e0af8c5d0b1a0
PawelHarasiuk <pharasiuk01@gmail.com>
2024-10-21 04:43:03 +0200

    init frontend

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,3 +1,14 @@
-function change() {
-    document.getElementById("hello").innerHTML = "Hello world"
-}
+document.getElementById("emailForm").addEventListener('submit', function (event) {
+    event.preventDefault();
+    let url = "https://5rrn370t09.execute-api.eu-central-1.amazonaws.com/default/test-cors";
+    const formData = new FormData(this)
+    let action = document.activeElement.value
+    let method = action === "create" ? "POST" : "DELETE"
+    fetch(url + action, {
+        method: method,
+        body: formData
+    })
+        .then(r => r.text())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+})
