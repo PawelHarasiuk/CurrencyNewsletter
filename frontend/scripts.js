f6e2909a82afcb9f06d25dfca26017c1d39ff4de
PawelHarasiuk <pharasiuk01@gmail.com>
2024-10-23 17:56:22 +0200

    updated frontend

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,14 +1,29 @@
-document.getElementById("emailForm").addEventListener('submit', function (event) {
-    event.preventDefault();
-    let url = "https://5rrn370t09.execute-api.eu-central-1.amazonaws.com/default/test-cors";
-    const formData = new FormData(this)
-    let action = document.activeElement.value
-    let method = action === "create" ? "POST" : "DELETE"
-    fetch(url + action, {
-        method: method,
-        body: formData
-    })
-        .then(r => r.text())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-})
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("emailForm").addEventListener('submit', function (event) {
+        event.preventDefault();
+        let url = "https://uo9lbqpwoi.execute-api.eu-central-1.amazonaws.com/newsletter-api/";
+        const formData = new FormData(this);
+        let jsonData = {};
+
+        formData.forEach((value, key) => {
+            jsonData[key] = value;
+        });
+
+        let action = event.submitter.value;
+        let method = action === "create" ? "POST" : "DELETE";
+        let feedback = document.getElementById('feedback');
+
+        fetch(url + action, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(jsonData)
+        })
+            .then(r => r.text())
+            .then(data => console.log(data))
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
+    });
+});
